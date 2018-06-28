92ef47c573b06d27e8772b600424e5222659644f
SparksFullStack <csparks@antioch.edu>
2018-06-28 12:09:35 -0700

    main project finished--working on stretch goals

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,18 +1,58 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import axios from "axios";
+import { Route } from "react-router-dom";
 import './App.css';
 
+import Friends from "./components/Friends/Friends";
+import AddFriend from "./components/AddFriend/AddFriend"
+
 class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      friends: [],
+      friendId: 6,
+    }
+  }
+
+  componentWillMount(){
+    axios.get("http://localhost:5000/friends")
+    .then(res => {
+      this.setState({friends: res.data})
+    })
+    .catch(err => {throw err});
+  }
+
+  handlePost = (newName, newAge, newEmail) => {
+    let newId = this.state.friendId + 1;
+    axios.post("http://localhost:5000/friends", {
+        name: newName,
+        age: newAge,
+        email: newEmail,
+        id: newId,
+    })
+    .then(res => {
+      console.log(res);
+      this.setState({
+        friends: res.data,
+        friendId: newId,
+      })
+    })
+    .catch(err => {throw err});
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Route path="/" exact render={() => {
+          return (
+            <div className="App__main">
+              <Friends friendsData={this.state.friends} />
+              <AddFriend handlePost={this.handlePost} />
+            </div>
+          ) 
+        }} />
       </div>
     );
   }
